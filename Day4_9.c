c8c01f041e16450f97093e6c7a78918b4fdb7a4d
Susheel rai <97819258+mesusheel01@users.noreply.github.com>
2022-09-29 13:16:04 +0530

    Add files via upload

---
+++ Day4_9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int gcd(int x,int y)
+{
+      if(x==0)
+    return x;
+    else if(y==0)
+    return y;
+    else if(x>y)
+    return(x-y,y);
+    return(x,y-x);
+}
+void lcm(int x,int y)
+{
+   //this is the code for lcm of two numbers!
+
+}
+void main()
